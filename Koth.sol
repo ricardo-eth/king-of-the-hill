3acec26e76cbca1f2e7540751bf3d3315d189a9d
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-05-18 18:29:45 +0200

    UP

--- Koth.sol
+++ Koth.sol
@@ -1,67 +1,135 @@
 // SPDX-License-Identifier: MIT
 
-import "./Ownable.sol";
+/*
+  _  ___                      __   _   _            _    _ _ _ _ 
+ | |/ (_)                    / _| | | | |          | |  | (_) | |
+ | ' / _ _ __   __ _    ___ | |_  | |_| |__   ___  | |__| |_| | |
+ |  < | | '_ \ / _` |  / _ \|  _| | __| '_ \ / _ \ |  __  | | | |
+ | . \| | | | | (_| | | (_) | |   | |_| | | |  __/ | |  | | | | |
+ |_|\_\_|_| |_|\__, |  \___/|_|    \__|_| |_|\___| |_|  |_|_|_|_|
+                __/ |                                            
+               |___/                                             
+
+Repo: https://github.com/ricardo-eth/king-of-the-hill
+
+ * * 
+ *
+ * @title: KingOfTheHill
+ * @author: ricardo-eth
+ * @rules: The owner initiates an amount, then players must pay double the amount and wait for XX blocks defined by the creator to win.
+ * @notice The owner must initiate the pot at 1 gwei or more (constructor).
+ * 
+ * *
+*/
 
 pragma solidity ^0.8.0;
 
-contract Koth is Ownable {
+contract Koth {
     
-    // State variables
-    address private _winner;
+    // @dev state variables
     mapping(address => uint256) private _userBalances;
-    uint256 private _tax;
-    uint256 private _pot;
-    uint256 private _baseBlocks;
     uint256 private _blocks;
-    
-    // Events
+    uint256 private _base;
+    uint256 private _pot;
+    address private _owner;
+    address private _winner;
+    uint256 private _tax;
+    uint256 private _seedRest;
+
+    // @dev events
     event Deposit(address indexed account, uint256 amount);
-    
-    // Constructor
-    constructor(address owner_, uint256 tax_, uint256 pot_, uint256 howManyBlocks_) Ownable(owner_) payable {
-        require(tax_ >= 0 && tax_ <= 100, "Koth: Invalid percentage");
-        require(pot_ >= 0, "Koth: minimium balance require");
-        _baseBlocks = howManyBlocks_;
-        _blocks = block.number + _baseBlocks;
+    event Withdrew(address indexed account, uint256 amount);
+    event TimeReach(address indexed winner, uint256 amount);
+
+    /**
+      * @dev There are 1 important constructor : 
+      * - contractOwner_ : owner of the contract.
+      * - blocks_ : set the number of blocks before winning.
+      * - tax_ : percentage of the tax for the owner.
+      * - seedRestNextRound_ : percentage of what is left in the pot for the new round.
+     * */
+    constructor(address contractOwner_, uint256 blocks_, uint256 tax_, uint256 seedRestNextRound_) payable {
+        require(msg.value >= 1e9, "Koth: The owner must initiate the pot at 1gwei or more");
+        require(tax_ >= 0 && tax_ <= 15, "Koth: Invalid percentage");
+        require(seedRestNextRound_ >= 0 && seedRestNextRound_ <= 15, "Koth: Invalid percentage");
+        _blocks = block.number + blocks_;
+        _base = blocks_;
+        _owner = contractOwner_;
         _winner = msg.sender;
-        _tax = tax_; 
-        _pot = pot_;
+        _pot = msg.value;
+        _tax = tax_;
+        _seedRest = seedRestNextRound_;
     }
-    
-    // Modifiers
-    modifier potStart(){
-        require(msg.value >= 1e9,"Koth: send more than 1gwei");
+
+    // @dev modifiers
+    modifier onlyContractOwner() {
+        require(msg.sender == _owner, "Koth: Only contract owner can call this function.");
+        _;
+    }
+    modifier ownerCantPlay() {
+        require(msg.sender != _owner, "Koth: owner can not play");
+        _;
+    }
+    modifier depositDouble() {
+        require(msg.value >= _pot * 2, "Koth: send the double of the actual on the pot");
         _;
     }
     
-    modifier depositDouble(){
-        require(msg.value >= _pot * 2,"Koth: send the double of the actual balance");
+    modifier withdrawZero() {
+         require(_userBalances[msg.sender] > 0, "Koth: can not withdraw 0 ether");
         _;
     }
     
-    // Function declarations
-    function deposit() public payable potStart depositDouble {
+    // @dev functions
+    function deposit() public payable depositDouble ownerCantPlay{
         emit Deposit(msg.sender, msg.value);
+        _blocks = block.number + _base;
         _winner = msg.sender;
-        uint256 amount = msg.value - _pot * 2;
-        if (amount > 0) payable(msg.sender).transfer(amount);
-        _pot += msg.value - amount;
+        uint256 rest = msg.value - _pot * 2;
+        if (rest > 0) payable(msg.sender).transfer(rest);
+        _pot += msg.value - rest;
     }
-    
-    function pot() public view returns (uint256) {
+
+    function withdraw() public {
+        uint256 amount = _userBalances[msg.sender];
+        emit Withdrew(msg.sender, amount);
+        _userBalances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function _timeReached() private {
+        if (block.number >= _blocks) {
+            uint256 _allTax = _tax + _seedRest;
+            emit TimeReach(_winner, _pot * (100 - _allTax) / 100);
+            _userBalances[_winner] = _pot * (100 - _allTax) / 100;
+            _userBalances[_owner] = _pot * (100 - _tax) / 100;
+            _pot = _seedRest / 100;
+            _blocks = block.number + _base;
+        }
+    }
+
+    function withdrawall() public onlyContractOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    // @dev getters
+    function getPot() public view returns (uint256) {
         return _pot;
     }
-    
-    function balance() public view returns (uint256) {
+
+    function getBalance() public view returns (uint256) {
         return _userBalances[msg.sender];
     }
-    
-    function tax() public view returns (uint256) {
-        return _tax;
-    }
-    
-    function winner() public view returns (address) {
+
+    function getWinner() public view returns (address) {
         return _winner;
     }
-    
+
+    function getBlocks() public view returns (uint256) {
+        return _blocks;
+    }
+
+    function getBlocknumber() public view returns (uint256) {
+        return block.number;
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# king-of-the-hill
+```js
+/*
+  _  ___                      __   _   _            _    _ _ _ _ 
+ | |/ (_)                    / _| | | | |          | |  | (_) | |
+ | ' / _ _ __   __ _    ___ | |_  | |_| |__   ___  | |__| |_| | |
+ |  < | | '_ \ / _` |  / _ \|  _| | __| '_ \ / _ \ |  __  | | | |
+ | . \| | | | | (_| | | (_) | |   | |_| | | |  __/ | |  | | | | |
+ |_|\_\_|_| |_|\__, |  \___/|_|    \__|_| |_|\___| |_|  |_|_|_|_|
+                __/ |                                            
+               |___/                                             
+
+ * * 
+ *
+ * @title: KingOfTheHill
+ * @author: ricardo-eth
+ * @rules: The owner initiates an amount, then players must pay double the amount and wait for XX blocks defined by the creator * to win.
+ * @notice The owner must initiate the pot at 1 gwei or more (constructor).
+ * 
+ * *
+
+*/
+```
